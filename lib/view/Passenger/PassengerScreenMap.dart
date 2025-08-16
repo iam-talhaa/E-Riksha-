5acbe2c2034984ee8a1cd7bf2bac244181edd612
unknown <tahakhan4141@gmail.com>
2025-08-16 23:00:49 +0500

    Added Current Location

--- lib/view/Passenger/PassengerScreenMap.dart
+++ lib/view/Passenger/PassengerScreenMap.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:e_riksha/res/GlassmorphicContainer.dart';
 import 'package:e_riksha/res/TransparentTextFormField.dart';
@@ -14,36 +15,36 @@ class Passengerscreenmap extends StatefulWidget {
 }
 
 class _PassengerscreenmapState extends State<Passengerscreenmap> {
+  double CurrentLat = 0.0;
+  double CurrentLon = 0.0;
   @override
   void initState() {
     super.initState(); // always call super.initState()
     _listOfMarkers.addAll(_Makers);
 
-    _getCurrentLocation().then((v) {
-      var currentlat = v.latitude;
-      var currentlon = v.longitude;
-    });
+    _getCurrentLocation().then((v) {});
 
     mycurrentLocation();
   }
 
   mycurrentLocation() {
     _getCurrentLocation().then((v) {
-      var currentlat = v.latitude;
-      var currentlon = v.longitude;
       setState(() {
-        _iCamera = CameraPosition(
+        CurrentLat = v.latitude;
+        CurrentLon = v.longitude;
+
+        CameraPosition _iCamera = CameraPosition(
           zoom: 14,
-          target: LatLng(currentlat, currentlon),
+          target: LatLng(CurrentLat, CurrentLon),
         );
       });
     });
   }
 
-  CameraPosition _iCamera = CameraPosition(
-    zoom: 14,
-    target: LatLng(34.0151, 71.5249),
-  );
+  // final CameraPosition _iCamera = CameraPosition(
+  //   zoom: 14,
+  //   target: LatLng(CurrentLat, Currentlon),
+  // );
 
   Future<Position> _getCurrentLocation() async {
     await Geolocator.requestPermission().then((v) {}).onError((e, s) {});
@@ -55,8 +56,8 @@ class _PassengerscreenmapState extends State<Passengerscreenmap> {
 
   TextEditingController _SearchPlaceController = TextEditingController();
 
-  List<Marker> _Makers = [
-    // Marker(markerId: MarkerId('1'), position: LatLng(34.01056, 71.79889)),
+  final List<Marker> _Makers = [
+    Marker(markerId: MarkerId('1'), position: LatLng(currentlat, 71.79889)),
     // Marker(markerId: MarkerId('2'), position: LatLng(34.0105556, 71.7963889)),
     // Marker(markerId: MarkerId('3'), position: LatLng(34.011, 71.796)),
     // Marker(markerId: MarkerId('4'), position: LatLng(34.00968, 71.79445)),
@@ -72,12 +73,22 @@ class _PassengerscreenmapState extends State<Passengerscreenmap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("CurrentLat: $CurrentLat, CurrentLon: $CurrentLon");
+
+          
+        },
+      ),
       body: SafeArea(
         child: Stack(
           children: [
             GoogleMap(
               markers: Set<Marker>.of(_listOfMarkers),
-              initialCameraPosition: _iCamera,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(CurrentLat, CurrentLon),
+                zoom: 20,
+              ),
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
